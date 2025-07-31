36886f0d2668dd9755a3149debc0139d63aec4ad
Kaan Karakas <kaankarakas@Kaan-MacBook-Pro.local>
2025-07-31 22:58:14 +0300

    fix: run app on 0.0.0.0 for Railway

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Merhaba, Railway çalışıyor!"
+    return "Merhaba, muzikido-api çalışıyor!"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=5000)
